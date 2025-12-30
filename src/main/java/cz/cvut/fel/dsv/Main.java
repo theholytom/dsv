02af7839b3c8efe3fe5d50de18399e1fcda10199
Tomáš Holý <holytom5@fel.cvut.cz>
2025-12-30 15:48:10 +0100

    constructing controller

--- src/main/java/cz/cvut/fel/dsv/Main.java
+++ src/main/java/cz/cvut/fel/dsv/Main.java
@@ -1,7 +1,10 @@
 package cz.cvut.fel.dsv;
 
+import cz.cvut.fel.dsv.controller.NodeController;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new NodeController();
     }
 }
