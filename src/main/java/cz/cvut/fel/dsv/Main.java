0f359dda327d5c86854f2ecf2ac05f8cacd190c3
Tomáš Holý <holytom5@fel.cvut.cz>
2025-12-30 17:22:23 +0100

    parameter fix

--- src/main/java/cz/cvut/fel/dsv/Main.java
+++ src/main/java/cz/cvut/fel/dsv/Main.java
@@ -5,6 +5,6 @@ import cz.cvut.fel.dsv.controller.NodeController;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        new NodeController();
+        new NodeController(9000);
     }
 }
