014d2e52b4be623c1a0cbd3e6314d3f449da360a
Tomáš Holý <holytom5@fel.cvut.cz>
2026-01-02 17:41:05 +0100

    adding custom constructor to message

--- src/main/java/cz/cvut/fel/dsv/message/Message.java
+++ src/main/java/cz/cvut/fel/dsv/message/Message.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.dsv.message;
 
+import cz.cvut.fel.dsv.node.Topology;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,23 @@ public class Message {
     private String targetId;
     private MessageType type;
     private String content;
+    private Topology topology;
+
+    public Message(String senderId, String targetId, MessageType type, String content) {
+        this.senderId = senderId;
+        this.targetId = targetId;
+        this.type = type;
+        this.content = content;
+        topology = null;
+    }
+
+    public Message(String senderId, String targetId, MessageType type, Topology topology) {
+        this.senderId = senderId;
+        this.targetId = targetId;
+        this.type = type;
+        this.content = "";
+        this.topology = topology;
+    }
 }
+
+
