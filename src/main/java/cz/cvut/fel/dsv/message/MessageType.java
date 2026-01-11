5f7b3ec31739ed8b7f3e71b67e8ec639c5d49f47
Tomáš Holý <holytom5@fel.cvut.cz>
2026-01-11 11:56:48 +0100

    adding message types

--- src/main/java/cz/cvut/fel/dsv/message/MessageType.java
+++ src/main/java/cz/cvut/fel/dsv/message/MessageType.java
@@ -3,7 +3,8 @@ package cz.cvut.fel.dsv.message;
 public enum MessageType {
     HEALTHCHECK,
     TOKEN,
-    WORK,
+    WORK_ASSIGNMENT,
+    WORK_REQUEST,
     JOIN,
     LEAVE,
     TOPOLOGY_UPDATE,
